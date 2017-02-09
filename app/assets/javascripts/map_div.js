51fe964b5b31723257704d315448ae1bfd441038
Eric <twinpaireric@gmail.com>
2017-02-08 22:14:06 -0800

    Added markup(index.html), stylesheets, images, javascript

---
+++ app/assets/javascripts/map_div.js
@@ -0,0 +1,27 @@
+/* google maps */
+
+ google.maps.event.addDomListener(window, 'load', initialize);
+
+function initialize() {
+
+  /* position Amsterdam */
+  var latlng = new google.maps.LatLng(34.0187371, -118.2474923);
+
+  var mapOptions = {
+    center: latlng,
+    scrollWheel: false,
+    zoom: 15
+  };
+  
+  var marker = new google.maps.Marker({
+    position: latlng,
+    url: '/',
+    animation: google.maps.Animation.DROP
+  });
+  
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  marker.setMap(map);
+
+};
+
+/* end google maps */
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,18 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( normalize.min.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( jquery.fancybox.css )
+Rails.application.config.assets.precompile += %w( flexslider.css )
+Rails.application.config.assets.precompile += %w( styles.css )
+Rails.application.config.assets.precompile += %w( queries.css )
+Rails.application.config.assets.precompile += %w( animate.min.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( jquery-1.11.2.min.js )
+Rails.application.config.assets.precompile += %w( jquery.fancybox.pack.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( scripts.js )
+Rails.application.config.assets.precompile += %w( jquery.flexslider-min.js )
+Rails.application.config.assets.precompile += %w( jquery.waypoints.min.js )
+Rails.application.config.assets.precompile += %w( map_div.js )
